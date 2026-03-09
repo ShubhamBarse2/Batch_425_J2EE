2ca9724bab849428d54163cfdc7e2461d4e8b129
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-03-09 13:11:48 +0530

    Add files via upload

---
+++ Day5_Hibernate/src/main/java/com/entity/Employee.java
@@ -0,0 +1,73 @@
+package com.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "empData")
+public class Employee { // Many
+
+	@Id
+	int empId;
+	String empName;
+	String city;
+
+	@ManyToOne
+	@JoinColumn(name = "deddd")
+	Department dept;
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(int empId, String empName, String city, Department dept) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.city = city;
+		this.dept = dept;
+
+	}
+
+	public Department getDept() {
+		return dept;
+	}
+
+	public void setDept(Department dept) {
+		this.dept = dept;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", city=" + city + ", dept=" + dept + "]";
+	}
+
+}
