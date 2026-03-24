ac07afa69ec1e52224d9cdbfd605972f1fb1f1a1
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-03-24 13:12:40 +0530

    Add files via upload

---
+++ SpringBootDay6/src/main/java/com/tka/SpringBootDay3Application.java
@@ -0,0 +1,14 @@
+package com.tka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDay3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDay3Application.class, args);
+		System.err.println("Application is started ...!");
+	}
+
+}
---
+++ SpringBootDay6/src/main/java/com/tka/controller/StudentController.java
@@ -0,0 +1,54 @@
+package com.tka.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tka.entities.Student;
+import com.tka.service.StudentService;
+
+@RestController
+public class StudentController {
+
+	@Autowired
+	StudentService service;
+
+	@PostMapping("/addstudent")
+	public String insertData(@RequestBody Student s) {
+		String msg = service.insertData(s);
+		return msg;
+	}
+
+	@DeleteMapping("/removestd/{id}")
+	public String deleteData(@PathVariable int id) {
+		String msg = service.deleteData(id);
+		return msg;
+	}
+
+	@PutMapping("/updatestd/{id}")
+	public String updateData(@RequestBody Student s, @PathVariable int id) {
+		String msg = service.updateData(s, id);
+		return msg;
+	}
+
+	@GetMapping("/fetchData")
+	public List<Student> getAllData() {
+		List<Student> list = service.getAllData();
+		return list;
+	}
+
+	@GetMapping("/singleRecord")
+	public Student getSingleData(@RequestParam int id) {
+		Student s1 = service.getSingleData(id);
+		return s1;
+	}
+
+}
